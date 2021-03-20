f0154504b7ced2f86db21da84f339f68047a37c7
pmarkowski <markowskipawel83@gmail.com>
2021-03-20 14:25:27 +0100

    Szkielet aplikacji spring boot

---
+++ src/main/java/com/sda/projekt43/Projekt43Application.java
@@ -0,0 +1,13 @@
+package com.sda.projekt43;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt43Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projekt43Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/projekt43/Projekt43ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.projekt43;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt43ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
