f140404676d32f2f39bc43dbade1cc5a79a45fc1
pmarkowski <markowskipawel83@gmail.com>
2021-03-21 11:51:47 +0100

    new endpoint for adding themes

---
+++ src/main/java/com/sda/projekt43/controller/ThemeController.java
@@ -0,0 +1,14 @@
+package com.sda.projekt43.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ThemeController {
+
+    @PostMapping("api/themes")
+    ResponseEntity<String> addNewTheme(String themeName) {
+        return ResponseEntity.ok("okok");
+    }
+}
